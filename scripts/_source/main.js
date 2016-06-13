626622ecb928b46e572c9c3a3b61d904782f29c9
Justin Wang <justin@oakmade.com>
2016-06-13 12:48:43 -0400

    Updated Introducing Siteleaf and 30 other files

--- scripts/_source/main.js
+++ scripts/_source/main.js
@@ -89,6 +89,37 @@ $(function(){
     $('.article__content').fitVids();
   };
 
+  // anchors
+  // -------
+
+  var anchorForId = function(id) {
+    var anchor = document.createElement("a");
+    anchor.className = "anchor";
+    anchor.href      = "#" + id;
+    anchor.innerHTML = "#";
+    return anchor;
+  };
+
+  var linkifyAnchors = function(level, containingElement) {
+    var headers = containingElement.getElementsByTagName("h" + level);
+    for (var h = 0; h < headers.length; h++) {
+      var header = headers[h];
+
+      if (typeof header.id !== "undefined" && header.id !== "") {
+        header.appendChild(anchorForId(header.id));
+      }
+    }
+  };
+
+  // Add header anchor links
+  var contentBlock = document.getElementsByClassName("article__content")[0];
+  if (!contentBlock) {
+    return;
+  }
+  for (var level = 1; level <= 6; level++) {
+    linkifyAnchors(level, contentBlock);
+  }
+
   // initialize
   // ----------
 
